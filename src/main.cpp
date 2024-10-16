0a2aa991afaf1b34b8842fb7e278f84239f7b049
Ostap Doroshenko <doroshtap@gmail.com>
2024-10-16 10:59:29 -0400

    ftxui template

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Metoxid!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    using namespace ftxui;
+
+    Element document =
+        hbox({
+            text("left")   | border,
+            text("middle") | border | flex,
+            text("right")  | border,
+        });
+
+    auto screen = Screen::Create(
+        Dimension::Full(),       // width
+        Dimension::Fit(document) // height
+    );
+
+    Render(screen, document);
+    screen.Print();
+
+    return EXIT_SUCCESS;
 }
