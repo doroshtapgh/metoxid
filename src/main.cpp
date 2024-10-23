81ba6928d8136c6dc8fdf28f2a23c37d3152fd8c
Shrey <shreyshingala1@gmail.com>
2024-10-23 10:51:13 -0400

    0

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,36 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
 #include <iostream>
 #include <fstream>
+#include <ncurses.h>
+#include <exiv2/exiv2.hpp>
+#include <gdal_priv.h>  // Include GDAL header
 
-int main(int argc, char* argv[]) {
-    std::cout << "hi";
+int main() {
+    std::cout << "RUNNDING" << "\n";
+    // Initialize GDAL
+    GDALAllRegister();
 
-    using namespace ftxui;
-
-    Element document =
-        hbox({
-            text("left")   | border,
-            text("middle") | border | flex,
-            text("right")  | border,
-        });
-
-    auto screen = Screen::Create(
-        Dimension::Full(),       // width
-        Dimension::Fit(document) // height
-    );
-
-    Render(screen, document);
-    screen.Print();
-
-    return EXIT_SUCCESS;
+    // Initialize the screen
+    initscr();
+    
+    // Get the screen size
+    int row, col;
+    getmaxyx(stdscr, row, col);
+    
+    // Print "Hello, World!" at the center of the screen
+    mvprintw(row / 2, (col - 12) / 2, "Hello, World!");
+    
+    // Refresh the screen to show the text
+    refresh();
+    
+    // Wait for user input
+    getch();
+    
+    // End the ncurses mode
+    endwin();
+    
+    return 0;
 }
+
+
+//g++ myprog.cpp -o myprog 
+//g++ -o main src/main.cpp -I/usr/local/include -L/usr/local/lib -lexiv2 -lncurses -lgdal
