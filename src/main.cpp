158e4cb7e21680cea263d815b43fd17f43233067
Ostap Doroshenko <doroshtap@gmail.com>
2024-10-23 10:41:22 -0400

    switch to ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,11 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
-#include <fstream>
+#include <ncurses/ncurses.h>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
 
-    using namespace ftxui;
-
-    Element document =
-        hbox({
-            text("left")   | border,
-            text("middle") | border | flex,
-            text("right")  | border,
-        });
-
-    auto screen = Screen::Create(
-        Dimension::Full(),       // width
-        Dimension::Fit(document) // height
-    );
-
-    Render(screen, document);
-    screen.Print();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
